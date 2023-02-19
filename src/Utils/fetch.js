8768a4275d1aea71816064f17242a00fe71eb606
christiandiloy <diloychristian@gmail.com>
2023-02-20 01:12:14 +0800

    revert

--- src/Utils/fetch.js
+++ src/Utils/fetch.js
@@ -1,33 +1,18 @@
 import {serverRoutes} from './const';
 
-export const RegisterAPI = async (username, password, fullName, email) => {
-  try {
-    const myHeaders = new Headers();
+export const RegisterAPI = (username, password, fullName, email) =>{
+    var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-    const raw = JSON.stringify({
-      "username": username,
-      "password": password,
-      "fullName": fullName,
-      "email": email
+    var raw = JSON.stringify({
+    "username": username,
+    "password": password,
+    "fullName": fullName,
+    "email": email,
     });
-    const requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw
     };
-    const response = await fetch(serverRoutes.Register, requestOptions);
-    if (response.ok) {
-      const result = await response.json();
-      if (result.success) {
-        return { success: true };
-      } else {
-        return { success: false, message: result.message };
-      }
-    } else {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-  } catch (error) {
-    console.error(`Error: ${error}`);
-    return { success: false, message: error.message };
-  }
-};
+    return fetch(serverRoutes.Register, requestOptions)
+}
