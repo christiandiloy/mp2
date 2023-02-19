241979c419e556fd9f3f6a2d325e8eccf82743af
christiandiloy <diloychristian@gmail.com>
2023-02-19 19:00:52 +0800

    test

--- src/Utils/fetch.js
+++ src/Utils/fetch.js
@@ -2,7 +2,7 @@ import {serverRoutes} from './const';
 
 export const RegisterAPI = (username, password, fullName, email) =>{
     var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Content-Type", "text/plain");
     var raw = JSON.stringify({
     "username": username,
     "password": password,
